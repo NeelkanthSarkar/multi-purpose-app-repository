1ec9aac2f757a1fd8bb4418908d4932cedd50d0f
Neelkanth Sarkar <neelkanthsarkar6@gmail.com>
2023-01-13 12:49:58 +0530

    Revert "first commit with successful integration with notepad app"
    This reverts commit 4d6c403a8d08f5266ba2c3ee56014bea02ca5e02.

--- src/main/java/com/mpapp/multipurposeapp/dba/notepad/NotepadJPARepository.java
+++
@@ -1,7 +0,0 @@
-package com.mpapp.multipurposeapp.dba.notepad;
-
-import com.mpapp.multipurposeapp.utilities.customDataType.NotepadFileType;
-import org.springframework.data.repository.CrudRepository;
-
-public interface NotepadJPARepository extends CrudRepository<NotepadFileType,Integer> {
-}
